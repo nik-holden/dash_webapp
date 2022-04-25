4dd0d54ecd29cbf0703a2c07dbf69cc77e8ffbf7
Nik Holden <nik.holden99@gmail.com>
2022-04-25 17:36:54 +1200

    revert app details

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import dash
 # Create Dash app
 
-dash_app = dash.Dash(__name__, suppress_callback_exceptions=True)
-app = dash_app.server
+app = dash.Dash(__name__, suppress_callback_exceptions=True)
+app_server = app.server
 
--- apps/line_monthly_rainfall.py
+++ apps/line_monthly_rainfall.py
@@ -1,11 +1,10 @@
 from dash import html
 from dash import dcc
-import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import plotly.express as px
 
 import common_functions as cf
-from app import dash_app
+from app import app
 
 #connection, sqlalchmey_engine = cf.azure_sql_db_connection()
 
@@ -45,7 +44,7 @@ layout = html.Div([
 ])
 
 # set up callback function
-@dash_app.callback(
+@app.callback(
     Output(component_id='total_daily_rain', component_property='figure'),
     [Input(component_id='station_ID', component_property='value')]
 )
