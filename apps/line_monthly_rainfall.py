2b9d34b11052326b9050ebff398ed3d8e550a23b
Nik Holden <nik.holden99@gmail.com>
2022-04-25 18:22:20 +1200

    updated

--- apps/line_monthly_rainfall.py
+++ apps/line_monthly_rainfall.py
@@ -4,7 +4,7 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 
 import common_functions as cf
-from app import app
+from app import dash_app
 
 #connection, sqlalchmey_engine = cf.azure_sql_db_connection()
 
@@ -44,7 +44,7 @@ layout = html.Div([
 ])
 
 # set up callback function
-@app.callback(
+@dash_app.callback(
     Output(component_id='total_daily_rain', component_property='figure'),
     [Input(component_id='station_ID', component_property='value')]
 )
