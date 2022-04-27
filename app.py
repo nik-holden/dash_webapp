ac9eafa6d0c5fe984a255fc0f392b2e3680f79cf
Nik Holden <nik.holden99@gmail.com>
2022-04-27 15:26:05 +1200

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import dash
 # Create Dash app
 
-application = dash.Dash(__name__, suppress_callback_exceptions=True)
-server = application.server
+app = dash.Dash(__name__, suppress_callback_exceptions=True)
+server = app.server
 
