aa3802da16dbd56e45247614e637dfbf5542a2a2
Nik Holden <nik.holden99@gmail.com>
2022-05-07 19:27:48 +1200

    removed tile page

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import dash_bootstrap_components as dbc
 from graphs import line_monthly_rainfall
 from graphs import line_current_day_temp
 from graphs import line_daily_temp
-from graphs import tile_graphs
+#from graphs import tile_graphs
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -18,7 +18,7 @@ print('server ', callable(server))
 app.layout = html.Div([ 
     dcc.Location(id='url', refresh=False),
     html.Div([
-        dcc.Link('Multiple Graphs', href='/dash_apps/tile_graphs'),
+        #dcc.Link('Multiple Graphs', href='/dash_apps/tile_graphs'),
         dcc.Link('Total Monthly Rainfall', href='/dash_apps/line_monthly_rainfall'),
         dcc.Link('Current Day Temperature', href='/dash_apps/line_current_day_temp'),
         dcc.Link('Daily high-low Temperatures', href='/dash_apps/line_daily_temp')
@@ -38,8 +38,8 @@ def display_page(pathname):
         return line_current_day_temp.layout
     elif pathname == '/dash_apps/line_daily_temp':
         return line_daily_temp.layout
-    elif pathname == '/dash_apps/tile_graphs':
-        return tile_graphs.layout
+    #elif pathname == '/dash_apps/tile_graphs':
+    #    return tile_graphs.layout
     else:
         return line_monthly_rainfall.layout 
 # Run local app
