1cafc0ad0f9b55e24836285d0842051ea3b9541c
Nik Holden <nik.holden99@gmail.com>
2022-05-15 13:37:08 +1200

    add dual graph page

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from graphs import line_monthly_rainfall
 from graphs import line_current_day_temp
 from graphs import line_daily_temp
 from graphs import bar_day_rainfall
+from graphs import line_daily_temp_2_gphs
 #from graphs import tile_graphs
 
 app_ = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -23,7 +24,8 @@ app_.layout = html.Div([
         dcc.Link('Total Monthly Rainfall', href='/dash_app_s/line_monthly_rainfall'),
         dcc.Link('Daily Rainfall', href='/dash_app_s/bar_day_rainfall'),
         dcc.Link('Current Day Temperature', href='/dash_app_s/line_current_day_temp'),
-        dcc.Link('Daily high-low Temperatures', href='/dash_app_s/line_daily_temp')
+        dcc.Link('Daily high-low Temperatures', href='/dash_app_s/line_daily_temp'),
+        dcc.Link('Daily high-low Temperatures', href='/dash_app_s/line_daily_temp_2_gphs')
     ], className="row"),
     html.Div(id='page_content', children=[])
 ])
@@ -44,6 +46,8 @@ def display_page(pathname):
     #    return tile_graphs.layout
     elif pathname == '/dash_app_s/bar_day_rainfall':
         return bar_day_rainfall.layout
+    elif pathname == '/dash_app_s/line_daily_temp_2_gphs':
+        return line_daily_temp_2_gphs.layout
     else:
         return line_monthly_rainfall.layout 
 # Run local app_
---
+++ graphs/line_daily_temp_2_gphs.py
@@ -0,0 +1,92 @@
+#from turtle import width
+from dash import html, dcc, callback, Input, Output
+import plotly.express as px
+
+from common_functions import read_from_db
+
+
+sql_stmt = """
+SELECT
+CASE WHEN t2.station_owner IS NOT NULL THEN t2.station_owner ELSE t1.stationID END AS stationID
+,observation_date
+,metric_min_temp AS min_temperature
+,metric_max_temp AS max_temperature
+from weather.daily_weather_metrics t1
+JOIN weather.DIM_weatherstation_details t2 ON t1.stationID = t2.station_id
+ORDER BY stationID, observation_date
+"""
+
+daily_temp_df = read_from_db(sql_stmt)
+
+# Create list of station ID's
+station_list = [i for i in daily_temp_df['stationID'].unique()]
+
+# Add the 'All' values to the list of station
+station_list.append('All')
+
+station_list.sort()
+
+print(station_list)
+
+layout = html.Div([
+
+    html.H1('Daily high-low Temperatures'),
+
+    html.Div([
+        html.Div(dcc.Dropdown(
+            id='dt_station_ID',
+            clearable=False,
+            value='All',
+            options=[{'label': i, 'value': i} for i in station_list],
+            ),
+        )
+    ],className='row'),
+    dcc.Graph(id='min_daily_temp'),
+    dcc.Graph(id='max_daily_temp'),
+    dcc.Interval(
+        id='dt_1-minute-interval',
+        interval=60000 #60 seconds, 1 minutes
+    )
+])
+
+# set up callback function
+@callback(
+    Output(component_id='max_daily_temp', component_property='figure'),
+    Output(component_id='min_daily_temp', component_property='figure'),
+    Input(component_id='dt_station_ID', component_property='value')
+)
+
+def filtered_min_daily_temp(selected_stationID = 'All'):
+    if selected_stationID == selected_stationID:
+        filtered_dataframe = daily_temp_df
+    else:
+        filtered_dataframe = daily_temp_df[daily_temp_df['stationID'] == selected_stationID]
+    
+
+    filtered_max_daily_temp = filtered_dataframe.loc[:, filtered_dataframe.columns!='min_temperature']
+    max_line_fig = px.line()
+
+    max_line_fig = px.line(data_frame=filtered_max_daily_temp,
+                       x='observation_date',
+                       y='max_temperature',
+                       title=f'Current Days Max Temperatures: {selected_stationID}',
+                       color='stationID'
+                       )
+
+    max_line_fig.update_layout(yaxis_range=[-5, 35])
+
+    
+    filtered_min_daily_temp = filtered_dataframe.loc[:, filtered_dataframe.columns!='max_temperature']
+    
+    min_line_fig = px.line
+
+    min_line_fig = px.line(data_frame=filtered_min_daily_temp,
+                       x='observation_date',
+                       y='min_temperature',
+                       title=f'Current Days Min Temperatures: {selected_stationID}',
+                       color='stationID'
+                       )
+
+    min_line_fig.update_layout(yaxis_range=[-5, 35])
+
+    return min_line_fig, max_line_fig
